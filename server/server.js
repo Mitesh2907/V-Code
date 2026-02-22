b7a9cbf3c40239edcf70d6573594fb3bb9ac727f
Mitesh2907 <miteshnayi081@gmail.com>
2026-02-22 17:05:21 +0530

    server.js updated for versol

--- server/server.js
+++ server/server.js
@@ -111,7 +111,12 @@ io.on("connection", (socket) => {
 });
 
 /* ---------------- START SERVER ---------------- */
-const PORT = process.env.PORT || 5000;
+if(process.env.NODE_ENV !== "production"){
+  const PORT = process.env.PORT || 5000;
 server.listen(PORT,  () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+}
+
+export default server;
+
