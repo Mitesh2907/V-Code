25bd2566eb42f364bea4b314bc9da96507d36d2e
Mitesh2907 <miteshnayi081@gmail.com>
2026-03-26 18:15:24 +0530

    some changes

--- client/src/configs/videoSocket.js
+++ client/src/configs/videoSocket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const videoSocket = io("http://localhost:5000", {
+const videoSocket = io(import.meta.env.VITE_API_URL, {
   transports: ["websocket"],
 });
 
