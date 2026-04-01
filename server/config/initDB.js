4949bd00f228d1708b6330bb240137b99afbbca3
Mitesh2907 <miteshnayi081@gmail.com>
2026-04-01 16:55:08 +0530

    update initDB

--- server/config/initDB.js
+++ server/config/initDB.js
@@ -8,7 +8,7 @@ const initDB = async () => {
     await pool.query(`
       CREATE TABLE IF NOT EXISTS users (
         id SERIAL PRIMARY KEY,
-        full_name VARCHAR(100) NOT NULL,
+        fullName VARCHAR(100) NOT NULL,
         email VARCHAR(100) UNIQUE NOT NULL,
         password VARCHAR(255) NOT NULL,
         avatar VARCHAR(255),
