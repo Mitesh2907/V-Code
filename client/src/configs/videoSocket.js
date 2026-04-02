ec5cffeb74e0318ff48a9bc5a1b58dc757f8e159
Mitesh2907 <miteshnayi081@gmail.com>
2026-04-02 23:09:22 +0530

    fix videoSocket

--- client/src/configs/videoSocket.js
+++ client/src/configs/videoSocket.js
@@ -1,6 +1,12 @@
 import { io } from "socket.io-client";
 
-const videoSocket = io(import.meta.env.VITE_API_URL, {
+const API_URL = import.meta.env.VITE_API_URL;
+
+if (!API_URL) {
+  console.error("❌ VITE_API_URL missing");
+}
+
+const videoSocket = io(API_URL, {
   transports: ["websocket"],
 });
 
